7fba6f9ef363249c1e8c30f2001b44f88b7cbd33
scott <ligongzai@foxmail.com>
2024-02-26 16:56:18 +1300

    feat: set up the SCSS of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import './App.css'
 import { Provider } from 'react-redux'
 import store from './store'
 import { BrowserRouter } from 'react-router-dom'
 import RouterComponent from './routes'
 // import { DotLoading } from 'antd-mobile'; // mobile version
+import './App.scss'
 
 
 function App() {
 
   return (
     <Provider store={store}>
-      <div>
+      <div className='app-body'>
         <BrowserRouter>
           Hello World
           <RouterComponent />
